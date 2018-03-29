cdd1fd35f477ecb046d0d3fdad44d4fa750f2f7a
alexinea <forerunner@outlook.com>
2018-03-30 00:49:23 +0800

    Add workflow station

--- src/Cosmos.Walkers/Workflow/IWorkflowStation.cs
+++ src/Cosmos.Walkers/Workflow/IWorkflowStation.cs
@@ -1,6 +1,12 @@
-﻿namespace Cosmos.Walkers.Workflow {
+﻿using Cosmos.Walkers.Workflow.Nodes;
+using Cosmos.Walkers.Workflow.Nodes.BuildInNodes;
+
+namespace Cosmos.Walkers.Workflow {
     public interface IWorkflowStation<T> {
         T Id { get; }
         string Name { get; }
+        IFlowChartNode<T> Root { get; }
+        StartNode GetStarter();
+        WalkerContext ExportWalkerContext();
     }
 }
---
+++ src/Cosmos.Walkers/Workflow/WorkflowContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Cosmos.Walkers.Workflow {
+    public class WorkflowContext {
+        public WorkflowContext(WalkerContext context) {
+            WalkerContext = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public WalkerContext WalkerContext { get; }
+    }
+}
